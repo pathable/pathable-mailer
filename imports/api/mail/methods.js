6ace3340fd1126cdd4ad5bb7afb0feb0b94ffccb
Faisal Ahmad <faisal.idreesi@gmail.com>
2017-09-01 19:56:14 +0500

    linter

--- imports/api/mail/methods.js
+++ imports/api/mail/methods.js
@@ -64,7 +64,7 @@ const passwordReset = new ValidatedMethod({
 
     const subject = '[Pathable] Reset your password';
     const html = userMailers.passwordReset({ authUrl });
-    
+
     Email.send({ subject, to, from, html });
   },
 });
